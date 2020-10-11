a7290e053de0751ba0a56be264069db8b297171e
HYNIX-Jang <hynix_1@hynixlabs.com>
2020-10-11 22:50:28 +0900

    Add Intro section fullpage.js easing

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
 $(function () {
-    
+    $('#fullpage').fullpage({
+        scrollHorizontally: true,
+        autoScrolling: true,
+        navigation: true,
+        navigationPosition: 'right',
+        parallax: true,
+        easingcss3: 'cubic-bezier(0.55, 0.085, 0, 1)',
+        scrollingSpeed: 850
+    });
 })
